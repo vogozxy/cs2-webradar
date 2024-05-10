0e78b2b600a89b3ceca82a1f7ab367440e374a98
vogozxy <134561098+vogozxy@users.noreply.github.com>
2024-05-10 23:16:28 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -191,7 +191,7 @@ export default function Home() {
     };
 
     eventSource.onmessage = (event: MessageEvent) => {
-      const gameData: GameData = JSON.parse(event.data);
+      const gameData: GameData = JSON.parse(event?.data ?? null);
 
       setGameData(gameData);
       setCurrentMap(gameData?.map ?? "");
